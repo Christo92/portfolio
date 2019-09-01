c15a5f1e52a9d96172ff7e225f2205dd31ec1298
Christopher <christopher.loubassou@hotmail.fr>
2019-09-01 23:04:31 +0200

    Build production

---
+++ build/precache-manifest.76d4a5ef7e1d5cf9f9d78ca00306a383.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41ce3013457ff686b16faa1fb031bd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d51fbc63fb657e4869d",
+    "url": "/static/css/main.54a4f978.chunk.css"
+  },
+  {
+    "revision": "8fb758fa99b32877bdcd",
+    "url": "/static/js/2.6ddb8f20.chunk.js"
+  },
+  {
+    "revision": "2d51fbc63fb657e4869d",
+    "url": "/static/js/main.c898a360.chunk.js"
+  },
+  {
+    "revision": "c49018507a564c0ada45",
+    "url": "/static/js/runtime~main.b908f969.js"
+  },
+  {
+    "revision": "bcc145ca77fc4e3893e31a47c01dae48",
+    "url": "/static/media/envelope.bcc145ca.svg"
+  },
+  {
+    "revision": "f9f02ed05aa86534c3842d44cb20d6c4",
+    "url": "/static/media/futura_book.f9f02ed0.ttf"
+  },
+  {
+    "revision": "98d2f97305ab25d4511982e73740922e",
+    "url": "/static/media/futura_light.98d2f973.ttf"
+  },
+  {
+    "revision": "ee64fb9d3f1ba2333e1b489283925bce",
+    "url": "/static/media/futura_medium.ee64fb9d.ttf"
+  },
+  {
+    "revision": "d213ac15252f666dcabb7acd51ee9f99",
+    "url": "/static/media/linkedin-logo.d213ac15.svg"
+  },
+  {
+    "revision": "cfdef02ba30ceaf3667657d73923cc4b",
+    "url": "/static/media/logo-animate.cfdef02b.svg"
+  },
+  {
+    "revision": "64febe6b8fd7e4410b5763f20d32e544",
+    "url": "/static/media/logo.64febe6b.svg"
+  },
+  {
+    "revision": "d3941c0e629f00d54d5dc0f6302552e2",
+    "url": "/static/media/menu-background.d3941c0e.jpg"
+  },
+  {
+    "revision": "cd16f7364fad67f282aa7b9abe9ecd9a",
+    "url": "/static/media/project-incoming.cd16f736.jpg"
+  },
+  {
+    "revision": "9de509e9aea1d7bf9ae8948306ab1c91",
+    "url": "/static/media/smart-tribune.9de509e9.jpg"
+  }
+]);
