0822216dddf061abf5a383885689b2b7538ea487
Christopher <christopher.loubassou@hotmail.fr>
2019-11-01 17:00:32 +0100

    Change home component to hook

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 // Remote packages
 import { Redirect } from 'react-router';
 import { SwishSpinner } from 'react-spinners-kit';
@@ -13,63 +13,56 @@ const fadeInAnimations = keyframes`${fadeIn}`;
 const FadeInDiv = styled.div`
     animation: 3s ${fadeInAnimations}
 `;
-class Loading extends Component {
 
-    state = {
-        done: false,
-        loading: true,
-        animation: false
-    }
+function Loading() {
 
-    componentDidMount() {
+    // Hooks state
+    const [done, setDone] = useState(false);
+    const [loading] = useState(true);
+    const [animation, setAnimation] = useState(false);
+
+    // Animation Loading
+    useEffect(() => {
         setTimeout(() => {
-            this.setState({
-                done: !this.state.done
-            })
+            setDone(true)
         }, 3000);
+    }, []);
 
-        this.handleAnimation();
-    }
-
-    handleAnimation = () => {
+    // Animation Writing
+    useEffect(() => {
         setTimeout(() => {
-            this.setState({
-                animation: !this.state.animation
-            })
+            setAnimation(true)
         }, 7500);
-    }
+    }, []);
 
-    goHome = () => {
-        if (this.state.animation === true) {
+    // Redirect to home page
+    function goHome() {
+        if (animation === true) {
             return (
                 <Redirect to="/home" />
             )
         }
     }
 
-    render() {
-        const { loading } = this.state;
+    return (
+        <div className="loading">
+            {!done ? (
+                <SwishSpinner
+                    size={30}
+                    color="#FFFFFF"
+                    loading={loading}
+                />
+            ) : (
+                    <div className="loading__container">
+                        <FadeInDiv>
+                            <img src={logoAnimate} className="loading__logo" alt="logo-loading" />
+                        </FadeInDiv>
+                    </div>
 
-        return (
-            <div className="loading">
-                {!this.state.done ? (
-                    <SwishSpinner
-                        size={30}
-                        color="#FFFFFF"
-                        loading={loading}
-                    />
-                ) : (
-                        <div className="loading__container">
-                            <FadeInDiv>
-                                <img src={logoAnimate} className="loading__logo" alt="logo-loading" />
-                            </FadeInDiv>
-                        </div>
-
-                    )}
-                {this.goHome()}
-            </div>
-        )
-    }
+                )}
+            {goHome()}
+        </div>
+    )
 }
 
 export default Loading;
